2c54ad55393d65d246d6b793161f857dcb0d2af3
Wabinab <chowjunwei37@gmail.com>
2021-04-11 02:01:11 +0000

    Another update from wabinab on progress on main file, move helper function to a helper file outside of jupyter notebook, and create another file to be edited with Quantum Key Distribution (QKD)

---
+++ helper/helper.py
@@ -0,0 +1,42 @@
+from qiskit import *
+
+
+def random_number_generator(num_qubits):
+    """
+    Generate and return a list of random numbers. 
+    """
+    n = num_qubits
+    
+    rnd_gen = QuantumCircuit(n)
+
+    rnd_gen.h(range(n - 1))
+
+    for i in range(n - 1):
+        rnd_gen.cnot(i, n - 1)  # Because qubit starts from 0 and ends at 19. 
+
+    rnd_gen.measure_all()
+    
+    answer = run_for_result(rnd_gen)
+    
+    random_numbers = [key for key in answer.keys()]
+    
+    return random_numbers
+
+
+def run_for_result(qc, backend=None):
+    """
+    Run on simulator and plot. 
+    :var qc: Quantum Circuit. 
+    """
+    
+    qobj = assemble(qc, shots=1000)
+    
+    if backend == None: 
+        backend = Aer.get_backend("qasm_simulator")
+    else:
+        backend = backend
+    
+    results = backend.run(qobj).result()
+    answer = results.get_counts()
+    
+    return answer
